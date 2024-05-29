3f3530f9b516dc39972934e1d00f03c1f79ea7e2
Mohd Ikram <ikram87388@gmail.com>
2024-05-29 18:34:53 +0530

    Modified QuickSort with easier implementation

--- quick_sort.py
+++ quick_sort.py
@@ -1,29 +1,16 @@
-def quick_sort(array):
-    def partition(arr, low, high):
-        pivot = arr[high]
-        left_wall = low - 1
-        
-        for i in range(low, high):
-            if arr[i] <= pivot:
-                left_wall += 1
-                arr[i], arr[left_wall] = arr[left_wall], arr[i]
-    
-
-        arr[left_wall + 1], arr[high] = arr[high], arr[left_wall+1]
-        return left_wall + 1
+from numpy import random
+arr = random.randint(1, 1000, size=50)
 
 
-    def quick_sort_helper(arr, low=0, high=None):
-        if not high:
-            high = len(arr) - 1
-            
-        if low < high:
-            pivot_index = partition(arr, low, high)
-            quick_sort_helper(arr, low, pivot_index - 1)
-            quick_sort_helper(arr, pivot_index + 1, high)
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less_than_pivot = [x for x in arr[1:] if  x<= pivot]
+        greater_than_pivot = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
 
-    quick_sort_helper(array, 0, len(array) - 1)
-    return array
 
-array = [11, 2, 5, 12, 6, 8, 7, 1, 9, 10, 3]
-print("Quick Sort:\n", quick_sort(array))
+print()
+print("Quick Sort", quick_sort(arr))
